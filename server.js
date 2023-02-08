de4b5a28b22d0100cdd7204cf5854f938388f607
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-08 22:19:16 +0600

    fixed2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.status(200).json({
     status: 'success',
     message: 'Hello world !',
