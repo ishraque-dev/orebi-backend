82d6559746f8bba009630ff5b5f336523ae30cee
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-08 22:33:00 +0600

    updated

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     message: 'Hello world !',
   });
 });
+app.get('/test', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Am from the test Route !',
+  });
+});
 app.listen(8000, () => {
   console.log('listening on 8000');
 });
