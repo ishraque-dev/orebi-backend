f83d4a59b8e7a77d087c82ae18494b806238a52a
ishraque-dev <ahamedesrak3@gmail.com>
2023-02-08 23:02:00 +0600

    route updated

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -8,12 +14,7 @@ app.get('/', (req, res) => {
     message: 'Hello world !',
   });
 });
-app.get('/test', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Am from the test Route !',
-  });
-});
+app.use('/api/v1/users', userRouter);
 app.listen(8000, () => {
   console.log('listening on 8000');
 });
